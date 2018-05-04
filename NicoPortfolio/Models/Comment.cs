2d5e43a175a78517c3f3fcbfe92095e3a1e24a78
vrnico <nico.daunt@gmail.com>
2018-05-04 10:53:46 -0700

    add comment and content models

---
+++ NicoPortfolio/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WeinerSales.Models
+{
+    [Table("Comment")]
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string Author{ get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ NicoPortfolio/Models/Content.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WeinerSales.Models
+{
+    [Table("Content")]
+    public class Content
+    {
+        [Key]
+        public int ContentId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
